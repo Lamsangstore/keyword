5e18addb88c8de022cc1197d2ee6f8c1756cb640
Lamsangstore <biiigooo76@gmail.com>
2026-03-26 15:56:47 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 // ✏️ แก้ไขค่านี้ให้ตรงกับ Firebase Project ของคุณ
 // วิธีหา: Firebase Console → Project Settings → Your Apps → Config
 
-window.FIREBASE_CONFIG = {
-  apiKey:            "YOUR_API_KEY",
-  authDomain:        "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL:       "https://YOUR_PROJECT_ID-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId:         "YOUR_PROJECT_ID",
-  storageBucket:     "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId:             "YOUR_APP_ID"
+const firebaseConfig = {
+  apiKey: "AIzaSyDfYW6HJ0zEKpdxWzAjNl28STy3l5yr2Rc",
+  authDomain: "lamsang-keyword.firebaseapp.com",
+  projectId: "lamsang-keyword",
+  storageBucket: "lamsang-keyword.firebasestorage.app",
+  messagingSenderId: "450847281629",
+  appId: "1:450847281629:web:241bcd0572fccf64b2e634",
+  measurementId: "G-TMRXT55984"
 };
